3001b083e16ca19073f0eefb4dab9da9ab2a0545
Rehan <muhammadrehan3024@gmail.com>
2024-11-20 21:00:39 +0500

    Implemented location accessing and used weatherapp Api

--- weather_app_gujrw/weatherapp/lib/screens/loading.dart
+++ weather_app_gujrw/weatherapp/lib/screens/loading.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
   const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+const LocationSettings locationSettings = LocationSettings(
+  accuracy: LocationAccuracy.high,
+  distanceFilter: 100,
+);
+
+const apiKey = '0c87fb8ee444ff1a89cfc638afbba244';
+
+Future<void> getLocation() async {
+  PermissionStatus permission = await Permission.location.request();
+
+  if (permission == PermissionStatus.granted) {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+
+      print("user's location: ${position.latitude}, ${position.longitude}");
+    } catch (e) {
+      print("Error fetching the location: $e");
+    }
+  } else if (permission == PermissionStatus.denied) {
+    print("Location permission denied");
+  } else if (permission == PermissionStatus.permanentlyDenied) {
+    print('Location permission permanently denied');
+    openAppSettings();
+  }
+}
+
+void getData() async {
+  final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=$apiKey'));
+
+  if (response.statusCode == 200) {
+    print('Data loaded successfully : ${response.body}');
+  } else {
+    print('Failed to laod data: ${response.statusCode} - ${response.body}');
+  }
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +70,9 @@ class Loading extends StatelessWidget {
       ),
       body: Center(
           child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                getData();
+              },
               child: const Text(
                 'Get Location',
                 style: TextStyle(color: Colors.black),
